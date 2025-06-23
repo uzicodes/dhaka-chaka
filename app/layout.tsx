db94a9fd124dee82c6c5064566e55243f4bd90ac
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-06-23 16:21:00 +0600

    favicon error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Materialize CSS and JavaScript */}
+        <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" async></script>
